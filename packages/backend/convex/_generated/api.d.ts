420614c767cf530fa912b5104fd8a85f99bf9589
Plamen Georgiev <plamen.georgiev@zigzag.global>
2025-09-15 10:39:26 +0300

    Add widget sessions

--- packages/backend/convex/_generated/api.d.ts
+++ packages/backend/convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as convex from "../convex.js";
+import type * as public_contactSessions from "../public/contactSessions.js";
 import type * as users from "../users.js";
 
 /**
@@ -26,6 +27,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   convex: typeof convex;
+  "public/contactSessions": typeof public_contactSessions;
   users: typeof users;
 }>;
 export declare const api: FilterApi<
